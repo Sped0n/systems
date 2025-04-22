57543c4824965a7b253a6cf8f25825653290154f
Sped0n <hi@sped0n.com>
2025-04-23 04:26:02 +0800

    fix: restructure Docker networking and firewall rules
    - Disable Docker's iptables and add manual NAT rules for eth0 and tailscale0
    - Update firewall rules to include additional network ranges for Docker host communication
    - Add cleanup commands for firewall rules on service stop
    - Add docker0 to trusted interfaces
    - Reorder allowed UDP ports list while keeping the same ports
    
    Signed-off-by: Sped0n <hi@sped0n.com>

--- machines/tennousei/services/docker.nix
+++ machines/tennousei/services/docker.nix
@@ -7,15 +7,43 @@
   virtualisation.docker = {
     enable = true;
     enableOnBoot = true;
+    daemon.settings = {
+      iptables = false;
+    };
   };
 
   networking.firewall = {
-    allowedUDPPorts = [21027 22000];
     allowedTCPPorts = [22000];
-    extraCommands = ''
-      iptables -I INPUT 1 -s 172.16.0.0/12 -p tcp -d 172.17.0.1 -j ACCEPT
-      iptables -I INPUT 2 -s 172.16.0.0/12 -p udp -d 172.17.0.1 -j ACCEPT
-    '';
+    allowedUDPPorts = [22000 21027];
+    extraCommands =
+      # For docker.host.internal
+      ''
+        iptables -I INPUT 1 -s 172.16.0.0/16 -p tcp -d 172.17.0.1 -j ACCEPT
+        iptables -I INPUT 2 -s 172.16.0.0/16 -p udp -d 172.17.0.1 -j ACCEPT
+        iptables -I INPUT 3 -s 192.168.0.0/20 -p tcp -d 172.17.0.1 -j ACCEPT
+        iptables -I INPUT 4 -s 192.168.0.0/20 -p udp -d 172.17.0.1 -j ACCEPT
+      ''
+      +
+      # Need to MASQUERADE after we set docker's iptables to false
+      ''
+        iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+        iptables -t nat -A POSTROUTING -o tailscale0 -j MASQUERADE
+      '';
+    extraStopCommands =
+      # For docker.host.internal
+      ''
+        iptables -D INPUT -s 172.16.0.0/16 -p tcp -d 172.17.0.1 -j ACCEPT || true
+        iptables -D INPUT -s 172.16.0.0/16 -p udp -d 172.17.0.1 -j ACCEPT || true
+        iptables -D INPUT -s 192.168.0.0/20 -p tcp -d 172.17.0.1 -j ACCEPT || true
+        iptables -D INPUT -s 192.168.0.0/20 -p udp -d 172.17.0.1 -j ACCEPT || true
+      ''
+      +
+      # Need to MASQUERADE after we set docker's iptables to false
+      ''
+        iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE || true
+        iptables -t nat -D POSTROUTING -o tailscale0 -j MASQUERADE || true
+      '';
+    trustedInterfaces = ["docker0"];
     checkReversePath = false;
   };
 
