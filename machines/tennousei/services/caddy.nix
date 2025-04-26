904124c5ec78f9172a3da436d8b6558a1ac81ec1
Sped0n <hi@sped0n.com>
2025-04-26 15:17:37 +0800

    feat: restructure Caddy config with new virtual host and proxies
    - Add tmpfiles rule to create /srv/eyes directory
    - Restructure Caddy virtual hosts configuration
    - Add new virtual host on port 10003 with redirect and reverse proxy rules
    - Configure reverse proxy for /tennousei and /tsuki paths
    - Add error handling for 404 responses in new virtual host
    
    Signed-off-by: Sped0n <hi@sped0n.com>

--- machines/tennousei/services/caddy.nix
+++ machines/tennousei/services/caddy.nix
@@ -5,14 +5,34 @@
 
   services.caddy = {
     enable = true;
-    virtualHosts."http://:10001" = {
-      extraConfig = ''
+    virtualHosts = {
+      "http://:10001".extraConfig = ''
         root * /srv/eyes
         file_server
         encode gzip zstd
         handle_errors {
           rewrite * /404.html
           file_server
+        }
+      '';
+
+      "http://:10003".extraConfig = ''
+        redir / /tennousei/index.html 301
+        redir /tennousei /tennousei/index.html 301
+        redir /tsuki /tsuki/index.html 301
+
+        handle_path /tennousei/* {
+          reverse_proxy http://localhost:8384 {
+            header_up Host {upstream_hostport}
+          }
+        }
+        handle_path /tsuki/* {
+          reverse_proxy http://100.122.137.55:8384 {
+            header_up Host {upstream_hostport}
+          }
+        }
+        handle {
+          respond 404
         }
       '';
     };
