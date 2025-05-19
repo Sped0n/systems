a8c59d332fefdf7ceb27c6b7ee733e37fa084735
Sped0n <hi@sped0n.com>
2025-05-19 18:47:19 +0800

    docs: add X server resolution and fcitx5 input source notes
    - Add 1920x1080 resolution configuration for X server
    - Add note about searching 'cn/pinyin' when manually adding input sources in fcitx5
    
    Signed-off-by: Sped0n <hi@sped0n.com>

--- modules/nixos/desktop/services/kbd.nix
+++ modules/nixos/desktop/services/kbd.nix
@@ -8,6 +8,9 @@
     enable = true;
     type = "fcitx5";
     fcitx5.addons = with pkgs; [
+      # NOTE:
+      # While manually adding input source in fcitx config
+      # GUI, search cn/pinyin instead of Chinese
       fcitx5-chinese-addons
       fcitx5-gtk
     ];
