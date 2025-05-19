055a1b94491ac9c6d5062bd0346cf15f64d14aeb
Sped0n <hi@sped0n.com>
2025-05-19 18:47:19 +0800

    docs: add X server resolution and fcitx5 input source notes
    - Add 1920x1080 resolution configuration for X server
    - Add note about searching 'cn/pinyin' when manually adding input sources in fcitx5
    
    Signed-off-by: Sped0n <hi@sped0n.com>

--- machines/shigoto/system.nix
+++ machines/shigoto/system.nix
@@ -19,4 +19,12 @@
 
   # Enable CUPS to print documents
   services.printing.enable = true;
+
+  # Display
+  services.xserver.resolutions = [
+    {
+      x = 1920;
+      y = 1080;
+    }
+  ];
 }
