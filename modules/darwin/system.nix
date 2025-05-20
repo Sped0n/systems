7aee1fc65968b2901f06eaf9fe8332e745e0428c
Sped0n <hi@sped0n.com>
2025-05-20 21:45:33 +0800

    feat: bump nix-darwin config to 25.05

--- modules/darwin/system.nix
+++ modules/darwin/system.nix
@@ -1,4 +1,4 @@
-{...}:
+{username, ...}:
 ###################################################################################
 #
 #  macOS's System configuration
@@ -12,11 +12,12 @@
 {
   system = {
     stateVersion = 6;
+    primaryUser = username;
     # activationScripts are executed every time you boot the system or run `nixos-rebuild` / `darwin-rebuild`.
-    activationScripts.postUserActivation.text = ''
+    activationScripts.postActivation.text = ''
       # activateSettings -u will reload the settings from the database and apply them to the current session,
       # so we do not need to logout and login again to make the changes take effect.
-      /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+      sudo -i -u ${username} /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
     '';
 
     defaults = {
