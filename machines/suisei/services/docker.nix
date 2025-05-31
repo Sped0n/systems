dd3b58041be95b105a9303b266467a36d1d7c173
Sped0n <hi@sped0n.com>
2025-05-31 20:49:10 +0800

    chore: add Docuum service for Docker image cleanup
    - Add Docuum service for Docker image eviction with 40GB threshold
    - Include Docuum in system packages
    - Configure systemd service for Docuum with Docker dependency
    
    Signed-off-by: Sped0n <hi@sped0n.com>

--- machines/suisei/services/docker.nix
+++ machines/suisei/services/docker.nix
@@ -1,5 +1,25 @@
-{
+{pkgs, ...}: let
+in {
   boot.kernel.sysctl = {
     "net.ipv4.ip_forward" = 1;
+  };
+
+  environment.systemPackages = [
+    pkgs.docuum
+  ];
+
+  systemd = {
+    services."docuum" = {
+      description = "Docuum: LRU eviction of Docker images";
+      wants = ["docker.service"];
+      after = ["docker.service"];
+      wantedBy = ["multi-user.target"];
+      path = with pkgs; [docker];
+      serviceConfig = {
+        User = "root";
+        ExecStart = "${pkgs.docuum}/bin/docuum --threshold 40GB";
+        Restart = "on-failure";
+      };
+    };
   };
 }
