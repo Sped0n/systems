0de6b88e7856a3148fec4d6d0ec06f8e22ae08ef
Sped0n <hi@sped0n.com>
2025-06-28 13:05:05 +0800

    chore: implement daily restic backup retention policy
    - Add retention policy for restic backups (keep 2 daily backups, remove weekly backups)
    
    Signed-off-by: Sped0n <hi@sped0n.com>

--- machines/suisei/services/restic.nix
+++ machines/suisei/services/restic.nix
@@ -6,6 +6,8 @@
   services.restic-backup = {
     enable = true;
     backupDir = "${home}/infra";
+    keepDaily = 2;
+    keepWeekly = 0;
     preBackupCommands = [
       "${pkgs.docker}/bin/docker compose -f ${home}/infra/docker-compose.yml stop"
     ];
