4fca685ff48111e50c62dcbb63c45632acf51b42
Sped0n <hi@sped0n.com>
2025-07-03 18:43:29 +0800

    fix: fix gnome custom keybinding setup
    Signed-off-by: Sped0n <hi@sped0n.com>

--- home/nixos/desktop/programs/gnome.nix
+++ home/nixos/desktop/programs/gnome.nix
@@ -34,6 +34,12 @@
         switch-input-source-backward = [];
       };
 
+      "org/gnome/settings-daemon/plugins/media-keys" = {
+        custom-keybindings = [
+          "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+        ];
+      };
+
       "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
         binding = "<Super>space";
         command = "albert toggle";
