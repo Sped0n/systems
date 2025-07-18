9e33c5d3cca75e8953d23bbaad12083745943016
Sped0n <hi@sped0n.com>
2025-07-19 00:26:42 +0800

    refactor: disable workaround for Go issue and clean up config
    - Disable a workaround related to a known Go issue by commenting out the QEMU overlay and associated package configuration
    - Simplify the configuration by removing an unused package import without altering functionality
    
    Signed-off-by: Sped0n <hi@sped0n.com>

--- machines/suisei/system.nix
+++ machines/suisei/system.nix
@@ -1,6 +1,6 @@
 {
-  pkgs-qemu8,
   modulesPath,
+  # pkgs-qemu8,
   ...
 }: {
   imports = [
@@ -18,13 +18,13 @@
     binfmt.emulatedSystems = ["x86_64-linux"];
   };
 
-  # see https://github.com/golang/go/issues/69255
-  nixpkgs.overlays = [
-    # NOTE: as for the naming, see
-    # - https://github.com/NixOS/nixpkgs/blob/a59eb7800787c926045d51b70982ae285faa2346/pkgs/applications/virtualization/qemu/default.nix#L140C3-L147C21
-    # - https://github.com/NixOS/nixpkgs/blob/b134951a4c9f3c995fd7be05f3243f8ecd65d798/pkgs/applications/virtualization/qemu/default.nix#L53C3-L57C45
-    (self: super: {qemu-user = pkgs-qemu8.qemu;})
-  ];
+  # # see https://github.com/golang/go/issues/69255
+  # nixpkgs.overlays = [
+  #   # NOTE: as for the naming, see
+  #   # - https://github.com/NixOS/nixpkgs/blob/a59eb7800787c926045d51b70982ae285faa2346/pkgs/applications/virtualization/qemu/default.nix#L140C3-L147C21
+  #   # - https://github.com/NixOS/nixpkgs/blob/b134951a4c9f3c995fd7be05f3243f8ecd65d798/pkgs/applications/virtualization/qemu/default.nix#L53C3-L57C45
+  #   (self: super: {qemu-user = pkgs-qemu8.qemu;})
+  # ];
 
   # builder setup
   nix = {
