ecb7abc9c288b9274894281399eccb66f0a05e9f
Sped0n <hi@sped0n.com>
2025-09-29 23:21:03 +0800

    feat: enhance git reset soft alias with commit preview
    Signed-off-by: Sped0n <hi@sped0n.com>

--- home/shared/programs/git.nix
+++ home/shared/programs/git.nix
@@ -47,7 +47,10 @@
       "gis" = "git status";
       "glo" =
         "git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --branches";
-      "grsh1" = "git reset --soft HEAD~1";
+      "grsh1" = ''
+        git --no-pager log -1 --pretty=format:"%C(bold yellow)Commit message about to be reset:%n%C(reset)%B" &&
+        git reset --soft HEAD~1
+      '';
       "gai" = "git add -i && git status";
       "gsu" = "git submodule update --init --recursive --progress";
     };
