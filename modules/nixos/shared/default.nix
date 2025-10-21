3ad5371707478417d6471edffb72161fb2e3d89c
Sped0n <hi@sped0n.com>
2025-10-21 16:38:28 +0800

    feat: add user wide Nix GC
    Signed-off-by: Sped0n <hi@sped0n.com>

--- modules/nixos/shared/default.nix
+++ modules/nixos/shared/default.nix
@@ -13,10 +13,18 @@
     ./docker.nix
   ];
 
+  # system wide gc
   nix.gc = {
     automatic = true;
     dates = lib.mkDefault "daily";
     options = lib.mkDefault "--delete-older-than 21d";
+  };
+
+  # user profile gc
+  systemd.user.services."nix-gc" = {
+    description = "Garbage collection for user profiles";
+    script = "/run/current-system/sw/bin/nix-collect-garbage --delete-older-than 21d";
+    startAt = "daily";
   };
 
   environment.systemPackages = [
