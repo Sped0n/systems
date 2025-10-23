0d85f7a9722e8dd5239249ffb34dc14da8d57f63
Sped0n <hi@sped0n.com>
2025-10-23 23:43:33 +0800

    feat: update neovim setups
    Signed-off-by: Sped0n <hi@sped0n.com>

--- home/shared/programs/neovim.nix
+++ home/shared/programs/neovim.nix
@@ -5,55 +5,53 @@
   ...
 }:
 {
-  home = {
-    packages =
-      with pkgs-unstable;
-      [
-        neovim
-      ]
-      ++
-        # Language supports
-        [
-          # nix
-          nixd
-          nixfmt
-
-          # toml
-          taplo
-
-          # yaml
-          yaml-language-server
-          prettierd
-
-          # json
-          vscode-langservers-extracted
-
-          # bash
-          bash-language-server
-          shellcheck
-          shfmt
-
-          # python
-          ruff
-          basedpyright
-          python313Packages.debugpy
-
-          # lua
-          lua-language-server
-          stylua
-          selene
-
-          # golang
-          gopls
-          delve
-          gofumpt
-
-          # markdown
-          marksman
-        ];
-    sessionVariables.EDITOR = "nvim";
+  programs.neovim = {
+    enable = true;
+    package = pkgs-unstable.neovim-unwrapped;
+    defaultEditor = true;
+    vimAlias = false;
   };
 
   xdg.configFile."nvim".source =
     config.lib.file.mkOutOfStoreSymlink "${home}/.config/systems/home/shared/config/nvim/";
+
+  # language supports
+  home.packages = with pkgs-unstable; [
+    # nix
+    nixd
+    nixfmt
+
+    # toml
+    taplo
+
+    # yaml
+    yaml-language-server
+    prettierd
+
+    # json
+    vscode-langservers-extracted
+
+    # bash
+    bash-language-server
+    shellcheck
+    shfmt
+
+    # python
+    ruff
+    basedpyright
+    python313Packages.debugpy
+
+    # lua
+    lua-language-server
+    stylua
+    selene
+
+    # golang
+    gopls
+    delve
+    gofumpt
+
+    # markdown
+    marksman
+  ];
 }
