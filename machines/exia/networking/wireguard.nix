33b84c649870de2a71375ea1f0321b021b86b02e
Sped0n <hi@sped0n.com>
2025-12-01 21:26:24 +0800

    refactor(exia): move wireguard cleanup to activation script
    Remove postDown from wg-quick and add system.activationScripts to
    delete wg0 interface if present during activation.
    
    Signed-off-by: Sped0n <hi@sped0n.com>

--- machines/exia/networking/wireguard.nix
+++ machines/exia/networking/wireguard.nix
@@ -15,11 +15,6 @@
     wg-quick.interfaces."wg0" = {
       configFile = config.age.secrets."wg0-conf".path;
       autostart = true;
-      postDown = ''
-        if ${pkgs.iproute2}/bin/ip link show wg0 >/dev/null 2>&1; then
-          ${pkgs.iproute2}/bin/ip link delete wg0
-        fi
-      '';
     };
 
     firewall = {
@@ -31,6 +26,12 @@
       checkReversePath = false;
     };
   };
+
+  system.activationScripts.fix-wireguard-activation = ''
+    if ${pkgs.iproute2}/bin/ip link show wg0 >/dev/null 2>&1; then
+      ${pkgs.iproute2}/bin/ip link delete wg0
+    fi
+  '';
 
   # https://rakhesh.com/linux-bsd/tailscale-wireguard-co-existing-or-i-love-policy-based-routing
   systemd.services.tailscaled.after = [ "wg-quick-wg0.service" ];
