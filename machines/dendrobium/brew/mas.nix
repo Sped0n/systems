f0aedff862ee229549191e5ad58d3b658c47cc1f
Sped0n <hi@sped0n.com>
2025-12-26 22:48:33 +0800

    chore(brew): remove Dropover MAS app, add keyboardcleantool cask
    Signed-off-by: Sped0n <hi@sped0n.com>

--- machines/dendrobium/brew/mas.nix
+++ machines/dendrobium/brew/mas.nix
@@ -2,7 +2,6 @@
 {
   homebrew.masApps = {
     "Quantumult X" = 1443988620;
-    "Dropover" = 1355679052;
     "Endel" = 1346247457;
     "VidHub" = 1659622164;
     "PDFgear" = 6469021132;
--- modules/darwin/brew/casks.nix
+++ modules/darwin/brew/casks.nix
@@ -13,6 +13,7 @@
     "rectangle"
     "appcleaner"
     "keka"
+    "keyboardcleantool"
 
     # entertainment
     "iina"
