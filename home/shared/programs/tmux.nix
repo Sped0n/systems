88d1b49a26a76fde4e1f88460d01904cba930faa
Sped0n <hi@sped0n.com>
2026-01-08 23:39:44 +0800

    feat(tmux): add fzf-powered pane searcher
    Signed-off-by: Sped0n <hi@sped0n.com>

--- home/shared/programs/tmux.nix
+++ home/shared/programs/tmux.nix
@@ -1,4 +1,37 @@
 { pkgs, ... }:
+let
+  tmuxPaneSearcher = pkgs.writeShellScriptBin "tmux-pane-searcher" ''
+    # 1. List all panes across all sessions.
+    # Format: "Session:Win.Pane <tab> WindowName <tab> Command <tab> Path"
+    # We use a distinct delimiter (tab) to make parsing easy.
+    PANES=$(tmux list-panes -a -F "#{session_name}:#{window_index}.#{pane_index}	#{window_name}	#{pane_current_command}	#{pane_current_path}")
+
+    # 2. Pipe to FZF
+    # --delimiter: split input by tabs
+    # --with-nth: show columns 1, 2, 3, 4 in the list
+    # --preview: tell tmux to capture the content of the pane ID (column 1) and print it in color (-e)
+    # --preview-window: put the preview on the right, taking 50% of space
+    TARGET=$(echo "$PANES" | ${pkgs.fzf}/bin/fzf \
+      --delimiter='\t' \
+      --with-nth=1,2,3,4 \
+      --header 'Session:Win.Pane | Name | Command | Path' \
+      --preview 'tmux capture-pane -e -p -t {1}' \
+      --preview-window='right:50%:wrap' \
+      --reverse \
+      --prompt 'Go to > ')
+
+    if [[ -n "$TARGET" ]]; then
+      # Extract the address (column 1) from the selected line
+      TARGET_ADDRESS=$(echo "$TARGET" | cut -f1)
+      
+      # Switch client to that pane
+      tmux switch-client -t "$TARGET_ADDRESS"
+      
+      # Also explicitly select the pane (sometimes switch-client just goes to the window)
+      tmux select-pane -t "$TARGET_ADDRESS"
+    fi
+  '';
+in
 {
   programs.tmux = {
     enable = true;
@@ -41,13 +74,16 @@
       bind - split-window -v -c '#{pane_current_path}'
       unbind '"'
       unbind %
-      bind -n M-h select-pane -L
-      bind -n M-l select-pane -R
-      bind -n M-k select-pane -U
-      bind -n M-j select-pane -D
+      bind h select-pane -L
+      bind l select-pane -R
+      bind k select-pane -U
+      bind j select-pane -D
 
       # --- Easy Reload ------------------------------------------------------
       bind r source-file ~/.config/tmux/tmux.conf \; display "Config reloaded!"
+
+      # --- Floating Pane Searcher -------------------------------------------
+      bind f display-popup -E -w 60% -h 70% "${tmuxPaneSearcher}/bin/tmux-pane-searcher"
 
       # --- Modifier Keys ----------------------------------------------------
       set -s extended-keys off
