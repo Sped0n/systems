84260fd5800737d445f36caec6615c324fcdbdf8
Sped0n <hi@sped0n.com>
2026-02-03 00:15:53 +0800

    refactor(swap): convert swap module to configurable service
    Signed-off-by: Sped0n <hi@sped0n.com>

--- modules/nixos/shared/system/swap.nix
+++ modules/nixos/shared/system/swap.nix
@@ -1,17 +1,35 @@
-{ lib, ... }:
+{ config, lib, ... }:
+let
+  my-swap = config.services.my-swap;
+in
 {
-  zramSwap = {
-    enable = true;
-    algorithm = "lz4";
-    memoryPercent = 50;
-    priority = 100;
+  options.services.my-swap = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Whether to enable the custom swap module.";
+    };
+
+    swapFileSize = lib.mkOption {
+      type = lib.types.int;
+      default = (1 * 1024);
+      description = "Size of the swap file in MiB.";
+    };
   };
 
-  swapDevices = [
-    {
-      device = "/swapfile";
-      size = lib.mkDefault (1 * 1024);
-      priority = 10;
-    }
-  ];
+  config = lib.mkIf my-swap.enable {
+    zramSwap = {
+      enable = true;
+      memoryPercent = 50;
+      priority = 100;
+    };
+
+    swapDevices = [
+      {
+        device = "/swapfile";
+        size = my-swap.swapFileSize;
+        priority = 10;
+      }
+    ];
+  };
 }
