2c569a78f31336bdb1c2e987e870cd3b0ecf44f0
Sped0n <hi@sped0n.com>
2026-02-08 21:02:06 +0800

    chore(neovim): remove codediff.nvim plugin
    Signed-off-by: Sped0n <hi@sped0n.com>

--- home/raw/neovim/lua/community.lua
+++ home/raw/neovim/lua/community.lua
@@ -10,7 +10,6 @@ return {
   -- editor
   { import = "astrocommunity.editing-support.conform-nvim" },
   { import = "astrocommunity.git.git-blame-nvim" },
-  { import = "astrocommunity.git.codediff-nvim" },
   { import = "astrocommunity.recipes.heirline-vscode-winbar" },
   -- languages
   { import = "astrocommunity.pack.lua" },
