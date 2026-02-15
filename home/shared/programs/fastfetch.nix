cf25f15499f96be1fa809df32637bdef0bdafc78
Sped0n <hi@sped0n.com>
2026-02-15 10:16:07 +0800

    fix(fastfetch): normalize spacing in packages format string
    Add missing space before "(brew-cask)" to maintain consistent spacing
    with other format strings.
    
    Signed-off-by: Sped0n <hi@sped0n.com>

--- home/shared/programs/fastfetch.nix
+++ home/shared/programs/fastfetch.nix
@@ -46,7 +46,7 @@
         {
           type = "packages";
           key = " {#39}│ {#32}Packages{#}  {#31}>{#}";
-          format = "{$1}  {?nix-system}{nix-system} (nix-system){?}{?brew-cask}, {brew-cask}(brew-cask){?}";
+          format = "{$1}  {?nix-system}{nix-system} (nix-system){?}{?brew-cask}, {brew-cask} (brew-cask){?}";
         }
         {
           type = "terminal";
