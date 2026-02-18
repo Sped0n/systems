04c6d4e4c7f8a7ea6a2e84eff6d587539051f289
Sped0n <hi@sped0n.com>
2026-02-18 22:38:00 +0800

    feat(nixos): enable timesyncd service for NTP synchronization
    Signed-off-by: Sped0n <hi@sped0n.com>

--- modules/nixos/shared/system/misc.nix
+++ modules/nixos/shared/system/misc.nix
@@ -4,6 +4,8 @@
   time.timeZone = "Asia/Singapore";
   # logrotate
   services.logrotate.enable = true;
+  # time sync
+  services.timesyncd.enable = true;
   # don't install the /lib/ld-linux.so.2 stub, this saves one instance of nixpkgs.
   environment.ldso32 = null;
   # ensure a clean & sparkling /tmp on fresh boots.
