0bce138805cb4f546696834e5d6c447f4c53af3c
Sped0n <hi@sped0n.com>
2026-03-01 19:01:27 +0800

    feat(esp-0): add NixOS configuration for ESP32 development
    Add system configuration for esp-0 machine with VirtualBox guest support,
    USB permissions for Espressif JTAG devices, and dialout/plugdev groups
    for microcontroller development.
    
    Signed-off-by: Sped0n <hi@sped0n.com>

---
+++ machines/esp-0/system/default.nix
@@ -0,0 +1,18 @@
+{ ... }:
+{
+  imports = [
+    ./usb.nix
+  ];
+
+  boot.initrd.availableKernelModules = [
+    "ata_piix"
+    "ohci_pci"
+    "ehci_pci"
+    "xhci_pci"
+    "ahci"
+    "sd_mod"
+    "sr_mod"
+  ];
+
+  virtualisation.virtualbox.guest.enable = true;
+}
---
+++ machines/esp-0/system/usb.nix
@@ -0,0 +1,28 @@
+{
+  pkgs,
+  vars,
+  ...
+}:
+{
+  users.groups = {
+    dialout = { };
+    plugdev = { };
+  };
+
+  users.users."${vars.username}" = {
+    extraGroups = [
+      "dialout"
+      "plugdev"
+    ];
+  };
+
+  services.udev = {
+    packages = [ pkgs.openocd ];
+    extraRules =
+      # Espressif USB JTAG/serial debug units
+      ''
+        ATTRS{idVendor}=="303a", ATTRS{idProduct}=="1001", MODE="660", GROUP="plugdev", TAG+="uaccess"
+        ATTRS{idVendor}=="303a", ATTRS{idProduct}=="1002", MODE="660", GROUP="plugdev", TAG+="uaccess"
+      '';
+  };
+}
