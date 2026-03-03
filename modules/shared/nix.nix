1a0cae22d12edec941fed847471839176331c165
Sped0n <hi@sped0n.com>
2026-03-03 17:34:30 +0800

    feat(nix): add log-lines, connect-timeout, and fallback settings
    Signed-off-by: Sped0n <hi@sped0n.com>

--- modules/shared/nix.nix
+++ modules/shared/nix.nix
@@ -5,33 +5,32 @@
   ...
 }:
 {
-  nix = {
-    settings = {
-      trusted-users = [
-        "${vars.username}"
-        "@admin"
-        "@wheel"
-      ];
-      experimental-features = "nix-command flakes";
-      download-buffer-size = 524288000;
-      http-connections = 64;
-      max-substitution-jobs = 64;
-      substituters = [ "https://cache.nixos.org?priority=10" ];
-      trusted-public-keys = [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" ];
-      extra-substituters = [
-        "https://nix-community.cachix.org?priority=50"
-        "https://install.determinate.systems?priority=60"
-        "https://cache.numtide.com?priority=70"
-      ];
-      extra-trusted-public-keys = [
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
-        "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
-      ];
-    };
-    extraOptions = ''
-      builders-use-substitutes = true
-    '';
+  nix.settings = {
+    trusted-users = [
+      "${vars.username}"
+      "@admin"
+      "@wheel"
+    ];
+    experimental-features = "nix-command flakes";
+    log-lines = 25;
+    download-buffer-size = 524288000;
+    http-connections = 64;
+    max-substitution-jobs = 64;
+    connect-timeout = 5;
+    fallback = true;
+    builders-use-substitutes = true;
+    substituters = [ "https://cache.nixos.org?priority=10" ];
+    trusted-public-keys = [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" ];
+    extra-substituters = [
+      "https://nix-community.cachix.org?priority=50"
+      "https://install.determinate.systems?priority=60"
+      "https://cache.numtide.com?priority=70"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
+      "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
+    ];
   };
 
   environment.systemPackages = [
