21b469e447f65ae1cf1f840ba12b35e7db5a9bdd
Sped0n <hi@sped0n.com>
2026-03-16 11:57:04 +0800

    refactor(esp-0): add solaar package for managing Logitech devices
    Signed-off-by: Sped0n <hi@sped0n.com>

--- machines/esp-0/home/packages.nix
+++ machines/esp-0/home/packages.nix
@@ -1,9 +1,12 @@
-{ pkgs, ... }:
+{ pkgs, pkgs-unstable, ... }:
 {
-  home.packages = with pkgs; [
-    nixos-rebuild-ng
-    chip-host-tools
-
-    popsicle
-  ];
+  home.packages =
+    (with pkgs; [
+      nixos-rebuild-ng
+      chip-host-tools
+    ])
+    ++ (with pkgs-unstable; [
+      popsicle
+      solaar
+    ]);
 }
