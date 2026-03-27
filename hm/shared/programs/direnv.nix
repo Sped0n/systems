a2009f0a8f5919b3806888af61464e002c48c8ab
Sped0n <hi@sped0n.com>
2026-03-27 10:26:51 +0800

    Revert "fix(direnv): use pkgs-unstable to workaround broken nixpkgs version"
    This reverts commit 6f5dab47b98f845c6227a0fe3343d24216f84b49.

--- hm/shared/programs/direnv.nix
+++ hm/shared/programs/direnv.nix
@@ -1,11 +1,8 @@
-{ pkgs-unstable, ... }:
+{ ... }:
 {
   programs = {
     direnv = {
       enable = true;
-      # TODO: direnv is broken on nixpkgs, and unstable merge the fix faster
-      #       see https://nixpk.gs/pr-tracker.html?pr=502832
-      package = pkgs-unstable.direnv;
       enableZshIntegration = true;
       nix-direnv.enable = true;
     };
