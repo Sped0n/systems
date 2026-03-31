ae4009ac49a6fff112229efe4ef4e2c05960e4b6
Sped0n <hi@sped0n.com>
2026-03-31 14:48:35 +0800

    refactor(cloudflared): simplify module structure and remove firewall rules
    Signed-off-by: Sped0n <hi@sped0n.com>

--- modules/nixos/server/services/cloudflared.nix
+++ modules/nixos/server/services/cloudflared.nix
@@ -11,21 +11,13 @@ let
   hostname = config.networking.hostName;
 in
 {
-  options.services.my-cloudflared = {
-    enable = lib.mkEnableOption "Enable Cloudflare Tunnel";
-  };
+  options.services.my-cloudflared.enable = lib.mkEnableOption "Enable Cloudflare Tunnel";
 
   config = lib.mkIf cfg.enable {
     age.secrets."${hostname}-cf-tunnel-json" = {
       file = "${secrets}/ages/${hostname}-cf-tunnel-json.age";
       owner = "root";
       mode = "0400";
-    };
-
-    # https://developers.cloudflare.com/cloudflare-one/networks/connectors/cloudflare-tunnel/configure-tunnels/tunnel-with-firewall/
-    networking.firewall = {
-      allowedTCPPorts = [ 7844 ];
-      allowedUDPPorts = [ 7844 ];
     };
 
     services.cloudflared = {
