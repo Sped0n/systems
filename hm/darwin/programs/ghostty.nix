77f3f6d14ca4634e0c922fe4ea644ca3fe7c369f
Sped0n <hi@sped0n.com>
2026-09-17 01:02:22 +0800

    hm/darwin/ghostty: switch auto-updates to tip channel
    Signed-off-by: Sped0n <hi@sped0n.com>

--- hm/darwin/programs/ghostty.nix
+++ hm/darwin/programs/ghostty.nix
@@ -3,5 +3,6 @@
   programs.ghostty = {
     enable = true;
     package = null; # currently pkgs.ghostty is marked as broken
+    settings.auto-update-channel = "tip"; # FIXME: workaround for https://github.com/ghostty-org/ghostty/issues/13070
   };
 }
